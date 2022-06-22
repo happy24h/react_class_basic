c698a2fd1d50a915423f827deebfcb3bba494b1b
@anhnv99 <vietanhhappy99@gmail.com>
2022-06-22 17:36:30 +0700

    lesson 13

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,24 +1,39 @@
 import React from "react";
 
-class ChildComponent extends React.Component {
+// class ChildComponent extends React.Component {
 
-    // re-render
-    render() {
-        console.log(">>> check props child: ", this.props)
+//     // re-render
+//     render() {
+//         console.log(">>> check props child: ", this.props)
 
-        // let jobs = this.props.jobs;
-        let {jobs} = this.props;
+//         // let jobs = this.props.jobs;
+//         let {jobs} = this.props;
 
-        return (
-            <>
-                <div className="jobs-list">
-                    {jobs.map((item, index) => (
-                        <div key = {index}>{item.position} - {item.salary}</div>
-                    ))}
-                </div> 
-            </>
-        )
-    }
+//         return (
+//             <>
+//                 <div className="jobs-list">
+//                     {jobs.map((item, index) => (
+//                         <div key = {index}>{item.position} - {item.salary}</div>
+//                     ))}
+//                 </div> 
+//             </>
+//         )
+//     }
+// }
+
+const ChildComponent = (props) => {
+
+    let {jobs} = props
+    return (
+        <>
+
+            <div className="jobs-list">
+                {jobs.map((item, index) => (
+                    <div key = {index}>{item.position} - {item.salary}</div>
+                ))}
+            </div> 
+        </>
+    )
 }
 
 export default ChildComponent;
